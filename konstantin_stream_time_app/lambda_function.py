2b108ef8a401a9f76d71025049bf3ceab817da99
Konstantin Zolozhkov <konstantinzolozhkov@Konstantins-MacBook-Air.local>
2022-12-12 13:24:19 +0200

    removed unused files

--- konstantin_stream_time_app/lambda_function.py
+++
@@ -1,9 +0,0 @@
-from corva import Api, Cache, Logger, StreamTimeEvent, stream
-
-from src.app import konstantin_stream_app
-
-@stream
-def lambda_handler(event: StreamTimeEvent, api: Api, cache: Cache):
-    """Insert your logic here"""
-    Logger.info('Hello, World!')
-    return konstantin_stream_app(event, api, cache)
